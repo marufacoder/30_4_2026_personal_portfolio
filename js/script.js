9fcd9221fdf859118d4f96cdc3e9d6df664079f3
marufacoder <marufacoder@gmailcom>
2026-04-30 12:51:52 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function UpdateDateTime(){
+    const now = new Date();
+    const updateDateTime = now.toLocaleString();
+    document.getElementById('datetime').textContent = updateDateTime;
+}UpdateDateTime();
+setInterval(UpdateDateTime,1000);
